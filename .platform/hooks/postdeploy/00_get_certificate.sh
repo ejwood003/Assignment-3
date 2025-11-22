3fb798eef5481bde07f536ab5e570c021a130488
ejwood003 <ejwood@byu.edu>
2025-11-21 22:41:04 -0700

    Change to certificate name

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Place in .platform/hooks/postdeploy directory
-sudo certbot -n -d section2group3.is404.net --nginx --agree-tos --email ejwood@byu.edu
+sudo certbot -n -d section2group3sc4.is404.net --nginx --agree-tos --email ejwood@byu.edu
